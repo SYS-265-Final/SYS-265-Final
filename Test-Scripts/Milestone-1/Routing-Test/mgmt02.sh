ab7bfc6d645569747aba655c1fb4db0894e20fa4
morganconnors <74949548+morganconnors@users.noreply.github.com>
2024-04-08 19:29:49 -0400

    Update mgmt02.sh

--- Test-Scripts/Milestone-1/Routing-Test/mgmt02.sh
+++ Test-Scripts/Milestone-1/Routing-Test/mgmt02.sh
@@ -1,14 +1,19 @@
 #!bin/bash
 
 echo "LAN to LAN connection test: "
+echo " "
 
 echo "MGMT01 (LAN) to MGMT02 (LAN)"
 ping -c 1 MGMT02
+echo " "
 
 echo "MGMT01 (LAN) to FW01 (LAN)"
 ping -c1 FW01
+echo " "
 
 echo "LAN to WAN connection test: "
+echo " "
 
 echo "MGMT01 (LAN) to champlain.edu (WAN)"
 ping -c 1 champlain.edu
+echo " "
