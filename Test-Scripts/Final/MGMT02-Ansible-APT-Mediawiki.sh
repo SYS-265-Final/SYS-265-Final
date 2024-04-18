9cf2fe54e64c57f6fba19959a8d71081896c7989
morganconnors <74949548+morganconnors@users.noreply.github.com>
2024-04-18 11:21:40 -0400

    Update and rename MGMT02-Ansible-Mediawiki.sh to MGMT02-Ansible-APT-Mediawiki.sh

--- Test-Scripts/Final/MGMT02-Ansible-APT-Mediawiki.sh
+++ Test-Scripts/Final/MGMT02-Ansible-APT-Mediawiki.sh
@@ -1,4 +1,4 @@
-sudo yum install docker -y
+sudo apt install docker docker-compose -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker run --name some-mediawiki -p 8080:80 -d mediawiki
